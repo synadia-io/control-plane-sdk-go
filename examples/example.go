184d29e448d86a5962096b12e468963d844260bd
caleblloyd <caleb@synadia.com>
2023-10-11 15:05:57 +0000

    🔄 synced local 'examples/' with remote 'go/cp-client/examples'

--- examples/example.go
+++ examples/example.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"log"
 
-	"github.com/synadia-io/control-plane-sdk-go/syncp"
+	scp "github.com/synadia-io/control-plane-sdk-go/syncp"
 )
 
 func main() {
-	client := syncp.NewAPIClient(syncp.NewConfiguration())
-	ctx := context.WithValue(context.Background(), syncp.ContextServerVariables, map[string]string{
+	client := scp.NewAPIClient(scp.NewConfiguration())
+	ctx := context.WithValue(context.Background(), scp.ContextServerVariables, map[string]string{
 		"baseUrl": "http://localhost:3000",
 	})
-	ctx = context.WithValue(ctx, syncp.ContextAccessToken, "uat_app_admin")
+	ctx = context.WithValue(ctx, scp.ContextAccessToken, "uat_app_admin")
 	accountList, _, err := client.SystemAPI.ListAccounts(ctx, "2Ui3Xj44Ly8BG13qQT0nQWzZ0ss").Execute()
 	if err != nil {
 		log.Fatal(err)
